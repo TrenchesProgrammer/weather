ccbcc1b89ef8b1cb5b29b8722a82fd72a821e99f
Heritage <ireanuoluwah@gmail.com>
2025-09-13 03:44:52 +0100

    added search and submit buttons

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex p-10 justify-between align-baseline">
-        <Image src="/logo.svg" alt="logo" width={150} height={100} />
+      <div className="flex lg:px-20 px-5 py-5 justify-between align-baseline">
+        <Image src="/logo.svg" alt="logo" width={150} height={150} />
         <button className="flex gap-2 bg-neutral-800 rounded-lg p-2.5">
           <Image src="/icon-units.svg" alt="units" width={16} height={16} />
           <p>Units</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import localFont from "next/font/local";
+import { DM_Sans, Bricolage_Grotesque } from "next/font/google";
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+const bricolageGrotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+});
 export default function Home() {
   return (
-   <>
-    
-   </>
+    <>
+      <div className="px-10 mt-10">
+        <h1
+          className={`text-5xl font-bold ${bricolageGrotesque.className} text-center `}
+        >
+          How's the sky looking today?
+        </h1>
+      </div>
+      <form className="flex justify-center gap-3 w-full  mt-12 flex-col sm:flex-row px-5 max-w-3xl m-auto">
+        <input type="text" className="bg-neutral-800 rounded-xl sm:w-[70%] w-[100%] bg-search  placeholder:text-neutral-200 px-12 p-3 " placeholder="Search for a place" />
+        <input type="submit" className="bg-[#4658D9] rounded-xl sm:w-[30%] w-[100%] p-3" value="Search" />
+      </form>
+    </>
   );
 }
