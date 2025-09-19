c3707ff5dfcd66e153728e6ae9bbe00b6ce4565b
Heritage <ireanuoluwah@gmail.com>
2025-09-19 22:20:03 +0100

    ui tweaks

--- Components/UnitDropdown.tsx
+++ Components/UnitDropdown.tsx
@@ -12,19 +12,19 @@ const UnitDropdown = ({imperial, setImperial,  mph, setMph, inch, setInch}:UnitD
     setImperial(!imperial);
   }
   return (
-    <div className="absolute rounded-lg p-3 flex-col gap-1 z-1 text-md  -bottom-87 flex text-left right-0 w-50 border border-neutral-600 bg-neutral-700">
-      <p className={`cursor-pointer bg-neutral-600 p-1 rounded-md`} onClick={handleImperial}>{`Switch to ${imperial?'metric':'imperial'}`}</p>
+    <div className="absolute rounded-lg p-3 flex-col gap-1 z-1 text-md  -bottom-87 flex text-left right-0 w-50 border border-neutral-700 bg-neutral-800">
+      <p className={`cursor-pointer bg-neutral-700 p-1 rounded-md`} onClick={handleImperial}>{`Switch to ${imperial?'metric':'imperial'}`}</p>
       <p className="text-neutral-400 text-sm">Temperature</p>
       <p>Celcius(&#176;C)</p>
       <p>Farenheit(&#176;F)</p>
-      <div className="w-full border-[0.1px] border-neutral-600" />
+      <div className="w-full border-[0.1px] border-neutral-700" />
       <p className="text-neutral-400 text-sm">Wind speed</p>
-      <p className={`cursor-pointer ${!mph?"bg-neutral-600":""} hover:bg-neutral-600 p-1 rounded-md`} onClick={()=>{setMph(false)}}>km/h</p>
-      <p className={`cursor-pointer ${mph?"bg-neutral-600":""} hover:bg-neutral-600 p-1 rounded-md`} onClick={()=>{setMph(true)}}>mph</p>
-      <div className="w-full border-[0.1px] border-neutral-600" />
+      <p className={`cursor-pointer ${!mph?"bg-neutral-700":""} hover:bg-neutral-700 p-1 rounded-md`} onClick={()=>{setMph(false)}}>km/h</p>
+      <p className={`cursor-pointer ${mph?"bg-neutral-700":""} hover:bg-neutral-700 p-1 rounded-md`} onClick={()=>{setMph(true)}}>mph</p>
+      <div className="w-full border-[0.1px] border-neutral-700" />
       <p className="text-neutral-400 text-sm ">Precipitation</p>
-      <p className={`cursor-pointer ${!inch ? "bg-neutral-600":""} hover:bg-neutral-600 p-1 rounded-md`} onClick={()=>{setInch(false)}}>Millimeters(mm)</p>
-      <p className={`cursor-pointer ${inch ? "bg-neutral-600":""} hover:bg-neutral-600 p-1 rounded-md`} onClick={()=>{setInch(true)}}>Inches(In)</p>
+      <p className={`cursor-pointer ${!inch ? "bg-neutral-700":""} hover:bg-neutral-700 p-1 rounded-md`} onClick={()=>{setInch(false)}}>Millimeters(mm)</p>
+      <p className={`cursor-pointer ${inch ? "bg-neutral-700":""} hover:bg-neutral-700 p-1 rounded-md`} onClick={()=>{setInch(true)}}>Inches(In)</p>
     </div>
   );
 };
